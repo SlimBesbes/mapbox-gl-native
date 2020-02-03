2a3a0b47a219099c6f5d464e2d04abcd2186d198
kevin <kevin.li@mapbox.com>
2020-02-03 16:53:42 +0800

    Create in expression head file

---
+++ include/mbgl/style/expression/in.hpp
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <mbgl/style/expression/expression.hpp>
+#include <mbgl/style/conversion.hpp>
+#include <memory>
+
+namespace mbgl {
+namespace style {
+namespace expression {
+
+class In : public Expression {
+public:
+    In(std::unique_ptr<Expression> needle_, std::unique_ptr<Expression> haystack_) :
+        Expression(Kind::In, bool),
+        needle(std::move(needle_)),
+        haystack(std::move(haystack_))
+    {}
+    
+    static ParseResult parse(const mbgl::style::conversion::Convertible& value, ParsingContext& ctx);
+    
+    EvaluationResult evaluate(const EvaluationContext& params) const override;
+    void eachChild(const std::function<void(const Expression&)>&) const override;
+
+    bool operator==(const Expression& e) const override {
+        if (e.getKind() == Kind::In) {
+            auto rhs = static_cast<const In*>(&e);
+            return *needle == *(rhs->needle) && *haystack == *(rhs->haystack);
+        }
+        return false;
+    }
+
+    std::vector<optional<Value>> possibleOutputs() const override {
+        return { nullopt };
+    }
+    
+    std::string getOperator() const override { return "in"; }
+
+private:
+    std::unique_ptr<Expression> needle;
+    std::unique_ptr<Expression> haystack;
+};
+
+} // namespace expression
+} // namespace style
+} // namespace mbgl
